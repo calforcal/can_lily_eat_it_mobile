8cf87fdc5f9fa534cb4427f8c436777657e271e7
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-05-21 07:17:43 -0600

    Add navigation to App, and created components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
+import * as React from 'react';
+import 'react-native-reanimated';
+import {NavigationContainer} from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={{
+          headerStyle: {
+            backgroundColor: 'royalblue',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+          <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Home'}} />
+          <Stack.Screen name="Search" component={SearchScreen} options={{title: 'Search'}} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
